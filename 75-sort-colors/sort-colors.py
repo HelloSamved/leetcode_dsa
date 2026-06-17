a63dcd1af6e7ad0e404321c2f126e01e5ad8dcee
HelloSamved <87407437+HelloSamved@users.noreply.github.com>
2026-06-18 04:04:44 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (89.98%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,4 +1,12 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        nums.sort()
-        return nums
+        a= nums.count(0)
+        b= nums.count(1)
+        c= nums.count(2)
+        nums.clear()
+        for i in range(a):
+            nums.append(0)
+        for i in range(b):
+            nums.append(1)
+        for i in range(c):
+            nums.append(2)
